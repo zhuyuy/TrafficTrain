4c4dffcc0e1cf45c717a36ea4dd04ddcae8b0746
jingyu <jingyuzhao@foxmail.com>
2020-07-01 23:13:41 +0800

    loginPage

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,13 @@
 from application import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World"
+
+
+@app.route('/login')
+def login():
+    return render_template('login-page.html')
