54cd0a823f7f4da760dd3b9b0206259988f4f8d2
jingyu <jingyuzhao@foxmail.com>
2020-07-02 10:44:00 +0800

    模板

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static')
+app.config.from_object(Config)
 
 from application import routes
---
+++ application/forms.py
@@ -0,0 +1,11 @@
+# 表单类，所有的表单都储存在这里
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('记住密码')
+    submit = SubmitField('登陆')
